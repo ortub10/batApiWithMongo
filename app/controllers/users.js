745bdd57982ecf162c80b857d47c039ec30615d0
ortub10 <ortub10@gmail.com>
2022-06-25 22:52:27 +0300

    more routes

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,4 +1,5 @@
 const User = require("../models/Users");
+const { use } = require("../routes/users");
 
 module.exports = {
   createUser: async (req, res) => {
@@ -42,6 +43,75 @@ module.exports = {
           new: true,
         }
       );
+      res.send(user);
+    } catch (e) {
+      res.send(e);
+    }
+  },
+  depositing: async (req, res) => {
+    try {
+      const userId = req.params.userId;
+      const acoountNumber = req.params.accountNumber;
+      const cashToAdd = req.body;
+      if (cashToAdd.cash < 0) return res.send("Nagetive naumber");
+      const user = await User.findOneAndUpdate(
+        { userId: userId, "accounts.numberAcount": acoountNumber },
+        { $inc: { "accounts.$.cash": cashToAdd.cash } },
+        {
+          new: true,
+        }
+      );
+
+      if (user === null) return res.send("User id or account number not exis ");
+      res.send(user);
+    } catch (e) {
+      res.send(e);
+    }
+  },
+
+  updateCredit: async (req, res) => {
+    try {
+      const userId = req.params.userId;
+      const acoountNumber = req.params.accountNumber;
+      const credit = req.body;
+      if (credit.credit < 0) return res.send("Nagetive naumber");
+      const user = await User.findOneAndUpdate(
+        { userId: userId, "accounts.numberAcount": acoountNumber },
+        { "accounts.$.credit": credit.credit },
+        {
+          new: true,
+        }
+      );
+      if (user === null) return res.send("User id or account number not exis");
+      res.send(user);
+    } catch (e) {
+      res.send(e);
+    }
+  },
+
+  cashWithdrawal: async (req, res) => {
+    try {
+      const userId = req.params.userId;
+      const acoountNumber = req.params.accountNumber;
+      const cashToWithdrawal = req.body;
+      const userCheck = await User.findOne({ userId: userId });
+      const acoountObj = userCheck.accounts.find(
+        (acoount) => acoount.numberAcount == acoountNumber
+      );
+
+      if (acoountObj.cash - cashToWithdrawal.cash < 0)
+        return res.send("The balance does not allow withdrawal");
+
+      if (cashToWithdrawal.cash < 0) return res.send("Nagetive naumber");
+      const user = await User.findOneAndUpdate(
+        { userId: userId, "accounts.numberAcount": acoountNumber },
+        { $inc: { "accounts.$.cash": cashToWithdrawal.cash * -1 } },
+        {
+          new: true,
+        }
+      );
+
+      if (user === null) return res.send("User id or account number not exis");
       res.send(user);
     } catch (e) {
       res.send(e);
--- app/routes/users.js
+++ app/routes/users.js
@@ -4,6 +4,9 @@ const {
   getUsers,
   getUser,
   addAccount,
+  depositing,
+  updateCredit,
+  cashWithdrawal,
 } = require("../controllers/users");
 
 const usersRouter = express.Router();
@@ -12,5 +15,8 @@ usersRouter.get("/", getUsers);
 usersRouter.get("/:userId", getUser);
 usersRouter.post("/", createUser);
 usersRouter.put("/add-account/:userId", addAccount);
+usersRouter.put("/cash/:userId/:accountNumber", depositing);
+usersRouter.put("/credit/:userId/:accountNumber", updateCredit);
+usersRouter.put("/withdrawal/:userId/:accountNumber", cashWithdrawal);
 
 module.exports = usersRouter;
